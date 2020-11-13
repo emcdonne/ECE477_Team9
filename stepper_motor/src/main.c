36d576b658042bf6fb67bfe41316b866ebcbb0ad
Brendan Murphy <bmurphy41999@gmail.com>
2020-11-12 19:45:13 -0500

    'we made progress'

--- stepper_motor/src/main.c
+++ stepper_motor/src/main.c
@@ -18,45 +18,29 @@
 
 #define Motor1Step GPIO_Pin_7 //PC7
 #define Motor1Dir GPIO_Pin_6 //PC6
-#define Motor1M0 GPIO_Pin_8 //PC8
-#define Motor1M1 GPIO_Pin_9 //PC9
-#define Motor1M2 GPIO_Pin_10 //PC10
-#define Motor1EN GPIO_Pin_11 //PC11
+#define Motor1EN GPIO_Pin_8 //PC8
 
-#define Motor2Step GPIO_Pin_1 //PC1
-#define Motor2Dir GPIO_Pin_2 //PC2
-#define Motor2M0 GPIO_Pin_3 //PC3
-#define Motor2M1 GPIO_Pin_6 //PF6
-#define Motor2M2 GPIO_Pin_0 //PC0
-#define Motor2EN GPIO_Pin_12 //PC12
+#define Motor2Step GPIO_Pin_1 //PB1
+#define Motor2Dir GPIO_Pin_0 //PB0
+#define Motor2EN GPIO_Pin_2 //PB2
 
-#define Motor3Step GPIO_Pin_11 //PB11
-#define Motor3Dir GPIO_Pin_12 //PB12
-#define Motor3M0 GPIO_Pin_15 //PB15
-#define Motor3M1 GPIO_Pin_14 //PB14
-#define Motor3M2 GPIO_Pin_13 //PB13
-#define Motor3EN GPIO_Pin_13 //PC13
+#define Motor3Step GPIO_Pin_10 //PC10
+#define Motor3Dir GPIO_Pin_9 //PC9
+#define Motor3EN GPIO_Pin_11 //PC11
 
-#define Motor4Step GPIO_Pin_6 //PB6
-#define Motor4Dir GPIO_Pin_7 //PB7
-#define Motor4M0 GPIO_Pin_8 //PB8
-#define Motor4M1 GPIO_Pin_9 //PB9
-#define Motor4M2 GPIO_Pin_10 //PB10
-#define Motor4EN GPIO_Pin_4 //PF4
+#define Motor4Step GPIO_Pin_3 //PA3
+#define Motor4Dir GPIO_Pin_2 //PA2
+#define Motor4EN GPIO_Pin_1 //PA1
 
-#define Motor5Step GPIO_Pin_12 //PA12
-#define Motor5Dir GPIO_Pin_15 //PA15
-#define Motor5M0 GPIO_Pin_3 //PB3
-#define Motor5M1 GPIO_Pin_4 //PB4
-#define Motor5M2 GPIO_Pin_5 //PB5
-#define Motor5EN GPIO_Pin_5 //PF5
+#define Motor5Step GPIO_Pin_4 //PB4
+#define Motor5Dir GPIO_Pin_3 //PB3
+#define Motor5EN GPIO_Pin_5 //PB5
 
 #define ToggleValue ((uint16_t) 3000)
 
 #define MotorGPIOA GPIOA
 #define MotorGPIOB GPIOB
 #define MotorGPIOC GPIOC
-#define MotorGPIOF GPIOF
 
 void micro_wait(unsigned int);
 
@@ -64,30 +48,12 @@ void micro_wait(unsigned int);
 GPIO_InitTypeDef MOTOR_A;
 GPIO_InitTypeDef MOTOR_B;
 GPIO_InitTypeDef MOTOR_C;
-GPIO_InitTypeDef MOTOR_F;
 
 TIM_TimeBaseInitTypeDef T1;
 TIM_TimeBaseInitTypeDef T2;
 TIM_TimeBaseInitTypeDef T3;
 TIM_TimeBaseInitTypeDef T4;
 TIM_TimeBaseInitTypeDef T5;
-
-/*void runMotor(char ingredient, int portion){
-    if(ingredient == "m"){
-
-    }
-    else if(ingredient == "p"){
-
-    }
-    else if(ingredient == "c"){
-
-    }
-    else if(ingredient == "o"){
-
-    }
-
-    return;
-}*/
 
 void motorSetup(void){
     //Initialize system and ensure core clock is up to date
@@ -98,47 +64,43 @@ void motorSetup(void){
          RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); //A
          RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE); //B
          RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE); //C
-         RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOF, ENABLE); //F
 
          //Enable Timers
          RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); //enable Timer 2 for Motor 1
          RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); //enable Timer 3 for Motor 2
          RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE); //enable Timer 6 for Motor 3
-         RCC_APB1PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE); //enable Timer 7 for Motor 4
+         RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE); //enable Timer 1 for Motor 4
          RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM14, ENABLE); //enable Timer 14 for Motor 5
 
          //Initialize MOTORS
-         MOTOR_A.GPIO_Pin = (Motor5Step | Motor5Dir);
-         MOTOR_B.GPIO_Pin = (Motor3Step | Motor3Dir | Motor3M0 | Motor3M1 | Motor3M2 | Motor4Step | Motor4Dir | Motor4M0 | Motor4M1 | Motor4M2 | Motor5M0 | Motor5M1 | Motor5M2);
-         MOTOR_C.GPIO_Pin = (Motor1Step | Motor1Dir | Motor1M0 | Motor1M1 | Motor1M2 | Motor1EN | Motor2Step | Motor2Dir | Motor2M0 | Motor2M2 | Motor2EN | Motor3EN);
-         MOTOR_F.GPIO_Pin = (Motor2M1 | Motor4EN | Motor5EN);
+         MOTOR_A.GPIO_Pin = (Motor4Step | Motor4Dir | Motor4EN);
+         MOTOR_B.GPIO_Pin = (Motor5Step | Motor5Dir | Motor5EN | Motor2Step | Motor2Dir | Motor2EN);
+         MOTOR_C.GPIO_Pin = (Motor1Step | Motor1Dir | Motor1EN | Motor3Step | Motor3Dir | Motor3EN);
 
          //Set pins to OUTPUT
          MOTOR_A.GPIO_Mode = GPIO_Mode_OUT;
          MOTOR_B.GPIO_Mode = GPIO_Mode_OUT;
          MOTOR_C.GPIO_Mode = GPIO_Mode_OUT;
-         MOTOR_F.GPIO_Mode = GPIO_Mode_OUT;
 
          //Complete Initialization
          GPIO_Init(MotorGPIOA, &MOTOR_A);
          GPIO_Init(MotorGPIOB, &MOTOR_B);
          GPIO_Init(MotorGPIOC, &MOTOR_C);
-         GPIO_Init(MotorGPIOF, &MOTOR_F);
 
          //Motor GPIOC Motor1EN
          GPIO_SetBits(MotorGPIOC, Motor1EN);
-         GPIO_SetBits(MotorGPIOC, Motor2EN);
+         GPIO_SetBits(MotorGPIOB, Motor2EN);
          GPIO_SetBits(MotorGPIOC, Motor3EN);
-         GPIO_SetBits(MotorGPIOF, Motor4EN);
-         GPIO_SetBits(MotorGPIOF, Motor5EN);
+         GPIO_SetBits(MotorGPIOA, Motor4EN);
+         GPIO_SetBits(MotorGPIOB, Motor5EN);
 
 
          //Set bits for DIR for all 5 motors
          GPIO_SetBits(MotorGPIOC, Motor1Dir);
-         GPIO_SetBits(MotorGPIOC, Motor2Dir);
-         GPIO_SetBits(MotorGPIOB, Motor3Dir);
-         GPIO_SetBits(MotorGPIOB, Motor4Dir);
-         GPIO_SetBits(MotorGPIOA, Motor5Dir);
+         GPIO_SetBits(MotorGPIOB, Motor2Dir);
+         GPIO_SetBits(MotorGPIOC, Motor3Dir);
+         GPIO_SetBits(MotorGPIOA, Motor4Dir);
+         GPIO_SetBits(MotorGPIOB, Motor5Dir);
 
 
          //Initialize and enable timer Motor1
@@ -203,15 +165,15 @@ void motorRun(int motorNum)
         dispenseInterval = 30000;
     }
     else if(motorNum == 2){ //motor 2 has peanuts
-        GPIO_ResetBits(MotorGPIOC, Motor2EN);
+        GPIO_ResetBits(MotorGPIOB, Motor2EN);
         dispenseInterval = 31000;
     }
     else if(motorNum == 4){ //motor 4 has cheerios
-        GPIO_ResetBits(MotorGPIOF, Motor4EN);
-        dispenseInterval = 310000;
+        GPIO_ResetBits(MotorGPIOA, Motor4EN);
+        dispenseInterval = 90000;
     }
     else if(motorNum == 5){ //motor 5 has granola
-        GPIO_ResetBits(MotorGPIOF, Motor5EN);
+        GPIO_ResetBits(MotorGPIOB, Motor5EN);
         dispenseInterval = 31000;
     }
     else{
@@ -225,7 +187,7 @@ void motorRun(int motorNum)
         for (j=0; j <= dispenseInterval; j++) //j <= 55000 for peanuts, j <= 30000 for M&Ms for 1 flap dispensing
         {
             if(motorNum == 1){
-                currentTimerVal = TIM_GetCounter(TIM2);
+                currentTimerVal = TIM_GetCounter(TIM3);
                 if(currentTimerVal > ToggleValue){
                     GPIO_SetBits(MotorGPIOC, Motor1Step);
                 }
@@ -242,46 +204,46 @@ void motorRun(int motorNum)
             else if(motorNum == 2){
                 currentTimerVal = TIM_GetCounter(TIM3);
                 if(currentTimerVal > ToggleValue){
-                    GPIO_SetBits(MotorGPIOC, Motor2Step);
+                    GPIO_SetBits(MotorGPIOB, Motor2Step);
                 }
                 else{
-                    GPIO_ResetBits(MotorGPIOC, Motor2Step);
+                    GPIO_ResetBits(MotorGPIOB, Motor2Step);
                 }
                 if(k == 0 || k == 3){
-                    GPIO_ResetBits(MotorGPIOC, Motor2Dir);
+                    GPIO_ResetBits(MotorGPIOB, Motor2Dir);
                 }
                 else if(k == 1 || k == 2){
-                    GPIO_SetBits(MotorGPIOC, Motor2Dir);
+                    GPIO_SetBits(MotorGPIOB, Motor2Dir);
                 }
             }
             else if(motorNum == 4){
-                currentTimerVal = TIM_GetCounter(TIM1);
+                currentTimerVal = TIM_GetCounter(TIM3);
                 if(currentTimerVal > ToggleValue){
-                    GPIO_SetBits(MotorGPIOB, Motor4Step);
+                    GPIO_SetBits(MotorGPIOA, Motor4Step);
                 }
                 else{
-                    GPIO_ResetBits(MotorGPIOB, Motor4Step);
+                    GPIO_ResetBits(MotorGPIOA, Motor4Step);
                 }
                 if(k == 0 || k == 3){
-                    GPIO_ResetBits(MotorGPIOB, Motor4Dir);
+                    GPIO_ResetBits(MotorGPIOA, Motor4Dir);
                 }
                 else if(k == 1 || k == 2){
-                    GPIO_SetBits(MotorGPIOB, Motor4Dir);
+                    GPIO_SetBits(MotorGPIOA, Motor4Dir);
                 }
             }
             else if(motorNum == 5){
-                currentTimerVal = TIM_GetCounter(TIM14);
+                currentTimerVal = TIM_GetCounter(TIM3);
                 if(currentTimerVal > ToggleValue){
-                    GPIO_SetBits(MotorGPIOA, Motor5Step);
+                    GPIO_SetBits(MotorGPIOB, Motor5Step);
                 }
                 else{
-                    GPIO_ResetBits(MotorGPIOA, Motor5Step);
+                    GPIO_ResetBits(MotorGPIOB, Motor5Step);
                 }
                 if(k == 0 || k == 3){
-                    GPIO_ResetBits(MotorGPIOA, Motor5Dir);
+                    GPIO_ResetBits(MotorGPIOB, Motor5Dir);
                 }
                 else if(k == 1 || k == 2){
-                    GPIO_SetBits(MotorGPIOA, Motor5Dir);
+                    GPIO_SetBits(MotorGPIOB, Motor5Dir);
                 }
             }
         }
@@ -293,13 +255,13 @@ void motorRun(int motorNum)
         GPIO_SetBits(MotorGPIOC, Motor1EN);
     }
     else if(motorNum == 2){ //motor 2 has peanuts
-        GPIO_SetBits(MotorGPIOC, Motor2EN);
+        GPIO_SetBits(MotorGPIOB, Motor2EN);
     }
     else if(motorNum == 4){ //motor 4 has cheerios
-        GPIO_SetBits(MotorGPIOF, Motor4EN);
+        GPIO_SetBits(MotorGPIOA, Motor4EN);
     }
     else if(motorNum == 5){ //motor 5 has granola
-        GPIO_SetBits(MotorGPIOF, Motor5EN);
+        GPIO_SetBits(MotorGPIOB, Motor5EN);
     }
 
 }
@@ -308,13 +270,14 @@ int main(void)
 {
     motorSetup();
 
-    /*motorRun(1);
-    micro_wait(1000000);
-    motorRun(2);
-    micro_wait(1000000);
-    motorRun(4);
-    micro_wait(1000000);*/
     motorRun(1);
+    micro_wait(100000);
+    motorRun(2);
+    micro_wait(100000);
+    motorRun(4);
+    micro_wait(100000);
+    motorRun(5);
+
     /*
      uint16_t CurrentTimerVal1 = 0;
      uint16_t CurrentTimerVal2 = 0;
